e62eba5bafc1058303edadea7c1bde58bbd08a5d
superman <goodnesskolapo@gmail.com>
2023-02-25 02:56:06 +0100

    omo

--- contracts/stake.sol
+++ contracts/stake.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./INft.sol";
 
-contract Nftstake {
+contract Nftstake{
 
      struct Staker{
         uint256 stakedId;
@@ -17,10 +17,43 @@ contract Nftstake {
     IERC721 stakeNft;
     uint256 constant Years = 31536000;
     IERC20 stakeToken;
+    address public owner;
+
+    
+    string private name;
+
+    
+    string private symbol;
+
+    
+    uint256 private decimal;
+
+    
+    uint private totalSupply;
+     mapping (address => uint256) private balanceOf;
+    // owner => spender =>  amount
+    mapping (address =>mapping(address => uint)) public allowance;
+
+    event transfer_(address indexed from, address to, uint amount);
+    event _mint(address indexed from, address to, uint amount);
+    IERC20 rewardToken;
 
     constructor() {
         stakeNft = IERC721(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
         stakeToken = IERC20(0x0D8775F648430679A709E98d2b0Cb6250d2887EF);
+        name = "ATOKEN";
+        symbol = "AT";
+        decimal = 1e18;
+        rewardToken = IERC20(address(this));
+        owner = msg.sender;
+
+        mint(address(this), 100000000000000000000000000);
+    }
+
+    modifier onlyowner {
+        require(msg.sender == owner, "unauthorised access");
+
+        _;
     }
 
 
@@ -66,4 +99,40 @@ contract Nftstake {
         _user.stakeAmount -= amount;
         stakeToken.transfer(msg.sender, amount);
     }
+
+      function WithdrawReward(uint256 amount) public {
+        Staker storage _user = user[msg.sender];
+        Reward();
+        uint256 _claimableReward = _user.rewardAccrued;
+        require(_claimableReward >= amount, "insufficient funds");
+        _user.rewardAccrued -= amount;
+        
+        rewardToken.transfer(msg.sender, amount);
+    }
+
+    function transfer(address _to, uint amount)public {
+        _transfer(msg.sender, _to, amount);
+        emit transfer_(msg.sender, _to, amount);
+
+    }
+      function _transfer(address from, address to, uint amount) internal {
+        require(balanceOf[from] >= amount, "insufficient fund");
+        require(to != address(0), "transferr to address(0)");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+    }
+
+    function mint(address to, uint256 amount) onlyowner public {
+        require(msg.sender == owner, "Access Denied");
+        require(to != address(0), "transfer to zero not possible");
+        totalSupply += amount;
+        balanceOf[to] += amount * decimal;
+        emit _mint(address(0), to, amount);
+
+
+    }
+     receive() external payable{}
+
+
+    fallback() external payable{}
 }
