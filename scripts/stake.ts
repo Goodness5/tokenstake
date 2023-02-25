8a2a903d49ad9e8f77636b784e53962240b47189
superman <goodnesskolapo@gmail.com>
2023-02-25 03:35:46 +0100

    this is done

--- scripts/stake.ts
+++ scripts/stake.ts
@@ -9,8 +9,16 @@ async function main() {
   console.log(`contract deployed at ${contract.address}`)
 
   const batholder = "0xe3444499a350cAF049f32781a29adB3B65A1E7ca";
+  const bataddress = "0x0D8775F648430679A709E98d2b0Cb6250d2887EF";
 
-  const erc20 = await ethers.getContractAt(IERC20, "0x0D8775F648430679A709E98d2b0Cb6250d2887EF")
+  const erc20 = await ethers.getContractAt("IERC20", bataddress);
+
+  const staker = await contract.connect(batholder);
+  const approve = await erc20.connect(batholder).approve(contract.address, 90);
+  const stake = await staker.stake(13);
+  const reward = await staker.Reward();
+  console.log(`current reward is ${reward}`);
+  console.log(stake);
 
 
 
