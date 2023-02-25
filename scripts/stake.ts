9554221d40a5f8c9f679bb0b6ee47e0c95a3fd2b
superman <goodnesskolapo@gmail.com>
2023-02-25 03:25:43 +0100

    this shit

--- scripts/stake.ts
+++ scripts/stake.ts
@@ -1,6 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
+    const [owner] = await ethers.getSigners();
+  
+  const Token = await ethers.getContractFactory("Nftstake");
+  const contract = await Token.deploy();
+  await contract.deployed();
+  console.log(`contract deployed at ${contract.address}`)
+
+  const batholder = "0xe3444499a350cAF049f32781a29adB3B65A1E7ca";
+
+  const erc20 = await ethers.getContractAt(IERC20, "0x0D8775F648430679A709E98d2b0Cb6250d2887EF")
+
+
 
 }
 
